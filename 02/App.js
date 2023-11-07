248c663996a65839e5deaf18f4675037f19e738b
AISHWARYAKHATRI <191200107021@asoit.edu.in>
2023-11-08 00:34:44 +0530

    React, Npm, Parcel, browserlist

--- 02/App.js
+++ 02/App.js
@@ -1,8 +1,19 @@
+// "use strict";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
   "h1",
   { id: "heading" },
-  "Hello World using React"
+  "Hello World using React createElement"
 );
+// a = 10;
+// console.log(a);
+// function a(a, b, ...args) {
+//   console.log(args.length);
+// }
+
+// a(1, 2, 3, 4, 5);
 
 const parent = React.createElement(
   "div",
@@ -11,7 +22,7 @@ const parent = React.createElement(
     React.createElement("div", { id: "child" }, [
       React.createElement(
         "h1",
-        { display: "flex", alignItems: "center", justifyContent: "center" },
+        { display: "flex", alignitems: "center", justifycontent: "center" },
         "Nested Rendering"
       ),
       heading,
---
+++ 02/README.md
@@ -0,0 +1,21 @@
+# Parcel
+
+- Dev build
+- Local server
+- HMR = Hot module replacement
+- File watching alogithm - written in C++
+- Caching - Faster builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential bundling - support older browsers
+- Diagnostic
+- Error handling and better error suggestions giving you a better developer experience
+- Host on HTTPS
+- Tree shaking - remove unused code
+- Different dev and prod bundles
+
+- CRA will automatically have all the above features.
