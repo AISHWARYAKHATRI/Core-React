8186751841c5cd8f4d124ae78932fa3f6e76c608
AISHWARYAKHATRI <191200107021@asoit.edu.in>
2024-01-15 00:10:27 +0530

    The diff between let, useState and useRef

--- 02/src/components/Header.js
+++ 02/src/components/Header.js
@@ -28,6 +28,9 @@ const Header = () => {
           <li>
             <Link to="/grocery">Grocery</Link>
           </li>
+          <li>
+            <Link to="/demo">Demo</Link>
+          </li>
           <li>Cart</li>
           <button
             className="login-btn"
